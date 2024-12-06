19f695c4692cb71c861d794ca5e7cfd9b54a5cdd
Yasin Kereci <kereciyasin52@gmail.com>
2024-12-06 23:33:51 +0100

    NEW

---
+++ AB01_03/AB01_03/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AB01_03
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
