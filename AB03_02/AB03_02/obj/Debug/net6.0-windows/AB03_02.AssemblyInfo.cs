d46274b16d596c356d1745c7878bb74a28669206
Yasin Kereci <kereciyasin52@gmail.com>
2024-12-09 00:38:28 +0100

    add

--- AB03_02/AB03_02/obj/Debug/net6.0-windows/AB03_02.AssemblyInfo.cs
+++ AB03_02/AB03_02/obj/Debug/net6.0-windows/AB03_02.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AB01_01")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AB03_02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce36f5f3759ea73dc583e13c4a211946e6f1fa7")]
-[assembly: System.Reflection.AssemblyProductAttribute("AB01_01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AB01_01")]
+[assembly: System.Reflection.AssemblyProductAttribute("AB03_02")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AB03_02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
